ee203e506e085c431f3d3833dc90002a47c59f98
Kévin Baumeyer <baumeyerkevin@gmail.com>
2017-05-18 12:40:03 +0200

    Brick drawing + rename to convention

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include "neslib.h"
 #define BALL_SPR 0x40
 
-static unsigned char i, j;
+static unsigned char i, j, k;
 static unsigned char pad,spr;
 
 //bar position
-static unsigned char bar_x;
-static unsigned char bar_y = 200;
+static unsigned char barX;
+static unsigned char barY = 200;
 //ball position & velocity
-static unsigned char ball_stuck = 1;
-static int ball_x;
-static int ball_y;
+static unsigned char ballStuck = 1;
+static int ballX;
+static int ballY;
 static int velocity;
 static int angle;
-static int velx, vely;
+static int velX, velY;
 //next ball position, used for the collision with the bar
 static int nbx, nby;
 //position of the ball on the bar (x)
 static int barBallPos;
+static int brickX, brickY;
 
 //Bar metasprite
 //x, y, sprite, palette
@@ -47,7 +48,7 @@ const unsigned char palSprites[16]={
 	0x0f,0x19,0x29,0x39
 };
 
-int aproxcos(int a) //(return val * 200)
+int aproxCos(int a) //(return val * 200)
 {
 	switch((a/10)%36)
 	{
@@ -111,7 +112,7 @@ int aproxcos(int a) //(return val * 200)
 	return 0;
 }
 
-int aproxsin(int a) //(return val * 200)
+int aproxSin(int a) //(return val * 200)
 {
 	switch(((a/10)+9)%36)
 	{
@@ -120,7 +121,7 @@ int aproxsin(int a) //(return val * 200)
 		case 18:
 			return 200;
 		default:
-			return -aproxcos(a + 90);
+			return -aproxCos(a + 90);
 	}
 }
 
@@ -129,58 +130,210 @@ int abs(int val)
 	return val < 0 ? val * -1 : val;
 }
 
+
+/*
+Background test
+*/
+static unsigned char updateList[3+32+3+8+1];//3 bytes address and length for 32 tiles, 3 bytes address and length for 8 attributes, end marker
+const unsigned char palette[16]={ 0x0f,0x16,0x26,0x36,0x0f,0x18,0x28,0x38,0x0f,0x19,0x29,0x39,0x0f,0x1c,0x2c,0x3c };
+
+#define BGPAL0	0x00	//bin 00000000
+#define BGPAL1	0x55	//bin 01010101
+#define BGPAL2	0xaa	//bin 10101010
+#define BGPAL3	0xff	//bin 11111111
+
+//metatile definitions, one metatile by another, 2 bytes per metatile
+const unsigned char metatiles[]={
+	0x00,0x00,0x00,0x00,//Empty
+	0xD0,0xD1,0xE0,0xE1,//Brick red
+	0xD0,0xD1,0xE0,0xE1,//Brick yellow
+	0xD0,0xD1,0xE0,0xE1,//Brick green
+	0xD0,0xD1,0xE0,0xE1//Brick blue
+};
+
+//metatile attributes, define which palette should be used for a metatile
+
+const unsigned char metaattrs[]={
+	BGPAL0,
+	BGPAL0,
+	BGPAL1,
+	BGPAL2,
+	BGPAL3
+};
+
+const unsigned char levelData[]={
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,//Minimal use line
+	0x01,0x00,0x00,0x01,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,
+	0x00,0x00,0x00,0x02,0x03,0x02,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x01,0x00,0x00,0x03,0x04,0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x02,0x01,0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x01,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x01,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x01,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00//End line
+};
+
+void prepareRowUpdate(unsigned char nameRow,unsigned int levelRow)
+{
+	static unsigned char i,tile,attr,tileOffset,updateNametableOffset,updateAttributeOffset,mask1,mask2,mask3,mask4;
+	static unsigned int nameAddress,attributeAddress;
+	static const unsigned char *src;
+
+	if(nameRow<30)//calculate tile and attribute row addresses in a nametable
+	{
+		nameAddress=NAMETABLE_A+(nameRow<<5);
+		attributeAddress=NAMETABLE_A+960+((nameRow>>2)<<3);
+	}
+	else
+	{
+		nameRow-=30;
+
+		nameAddress=NAMETABLE_C+(nameRow<<5);
+		attributeAddress=NAMETABLE_C+960+((nameRow>>2)<<3);
+	}
+
+	updateList[0]=MSB(nameAddress)|NT_UPD_HORZ;//set nametable update address
+	updateList[1]=LSB(nameAddress);
+
+	updateList[35]=MSB(attributeAddress)|NT_UPD_HORZ;//set attribute table update address
+	updateList[36]=LSB(attributeAddress);
+
+	updateNametableOffset=3;//offset in the update list for nametable data
+	updateAttributeOffset=3+32+3;//offset for attribute data
+
+	src=&levelData[levelRow<<4];//get offset in the level data
+
+	if(!(nameRow&1)) tileOffset=0; else tileOffset=2;//metatile data offset, 2 for odd row numbers
+
+	if(!(nameRow&2))//select appropriate masks for attribute bytes
+	{
+		mask1=0xfc;
+		mask2=0x03;
+		mask3=0xf3;
+		mask4=0x0c;
+	}
+	else
+	{
+		mask1=0xcf;
+		mask2=0x30;
+		mask3=0x3f;
+		mask4=0xc0;
+	}
+
+	for(i=0;i<8;++i)//as two neighborn attributes are grouped into a byte, the loop handles two metatiles in one iteration
+	{
+		tile=*src++;//get first metatile code
+
+		attr=(updateList[updateAttributeOffset]&mask1)|(metaattrs[tile]&mask2);//get metatile attribute, remember it
+
+		tile=(tile<<2)+tileOffset;//get metatile offset
+
+		updateList[updateNametableOffset+0]=metatiles[tile+0];//put top or bottom half of the first metatile into the update buffer
+		updateList[updateNametableOffset+1]=metatiles[tile+1];
+
+		tile=*src++;//get second metatile code
+
+		updateList[updateAttributeOffset++]=(attr&mask3)|(metaattrs[tile]&mask4);//get attribute, add the other attribute, put into update buffer
+
+		tile=(tile<<2)+tileOffset;//get metatile offset
+
+		updateList[updateNametableOffset+2]=metatiles[tile+0];//put top or bottom half of the second metatile into the update buffer
+		updateList[updateNametableOffset+3]=metatiles[tile+1];
+
+		updateNametableOffset+=4;
+	}
+}
+
+void preloadScreen(unsigned int levelY)
+{
+	static unsigned char i;
+
+	for(i=0;i<30;++i)
+	{
+		prepareRowUpdate(29-i,levelY>>4);//prepare a row, bottom to top
+
+		flush_vram_update(updateList);
+
+		levelY+=8;
+	}
+}
+
+
+
 void main(void)
 {
 	pal_spr(palSprites);
 
-	ppu_on_all();//enable rendering
-
 	//set initial coords
-	bar_x = 114;
-	ball_x = 1260;
-	ball_y = 1930;
+	barX = 114;
+	ballX = 1260;
+	ballY = 1930;
+
+	//Test background
+	pal_bg(palette);//set background palette from an array
+	pal_col(17,0x30);//white color for sprite
+
+	updateList[0]=0x20|NT_UPD_HORZ;//horizontal update sequence, dummy address
+	updateList[1]=0x00;
+	updateList[2]=32;//length of nametable update sequence
+
+	updateList[35]=0x20|NT_UPD_HORZ;
+	updateList[36]=0x00;
+	updateList[37]=8;//length of attribute update sequence
+
+	updateList[46]=NT_UPD_EOF;
+	preloadScreen(0);
+	//BG end
+
+	ppu_on_all();//enable rendering
 
 	while(1)
 	{
-		ppu_wait_frame();//wait for next frame
+		ppu_wait_nmi();//wait for next nmi
 		
 		spr = 0;
 
 		//display metasprite
 		//bar
-		spr = oam_meta_spr(bar_x, bar_y, spr, metaBar);
+		spr = oam_meta_spr(barX, barY, spr, metaBar);
 		//ball
-		spr = oam_spr((unsigned char)(ball_x/10), (unsigned char)(ball_y/10), BALL_SPR, 3, spr);
+		spr = oam_spr((unsigned char)(ballX/10), (unsigned char)(ballY/10), BALL_SPR, 3, spr);
         
-        spr = oam_meta_spr(0x20, 0x20, spr, metaBrickBlue);
+        //spr = oam_meta_spr(0x20, 0x20, spr, metaBrickBlue);
 
 		//poll pad
 		pad = pad_poll(0);
 
 		//Move bar
-		if(pad&PAD_LEFT && bar_x>= 1) 
+		if(pad&PAD_LEFT && barX>= 1) 
 		{
-			bar_x -= 3;
-			if(ball_stuck)
+			barX -= 3;
+			if(ballStuck)
 			{
-				ball_x -= 30;
+				ballX -= 30;
 			}
 		}
-		if(pad&PAD_RIGHT && bar_x<=222) 
+		if(pad&PAD_RIGHT && barX<=222) 
 		{
-			bar_x += 3;
-			if(ball_stuck)
+			barX += 3;
+			if(ballStuck)
 			{
-				ball_x += 30;
+				ballX += 30;
 			}
 		}
 
 		//Move ball
-		if(ball_stuck)
+		if(ballStuck)
 		{
 			if(pad&PAD_A)
 			{
-				ball_stuck = 0;
+				ballStuck = 0;
 				velocity = 10;//max 50
 				if(pad&PAD_LEFT)
 				{
@@ -199,25 +352,25 @@ void main(void)
 		else
 		{	
 			//window collision
-			if(ball_y < 80)//Top
+			if(ballY < 80)//Top
 			{
-				ball_y = 80;
+				ballY = 80;
 				angle = 360 - angle;
 			}
-			else if(ball_y > 2300)//Bottom
+			else if(ballY > 2300)//Bottom
 			{
-				ball_y = 2300;
+				ballY = 2300;
 				angle = 360 - angle;
 				//TODO loose
 			}
-			if(ball_x < 0)//Left
+			if(ballX < 0)//Left
 			{
-				ball_x = 0;
+				ballX = 0;
 				angle = 360 + 180 - angle;
 			}
-			else if(ball_x > 2480)//Right
+			else if(ballX > 2480)//Right
 			{
-				ball_x = 2480;
+				ballX = 2480;
 				angle = 360 + 180 - angle;
 			}
 			angle = (angle+720) % 360;
@@ -226,24 +379,24 @@ void main(void)
 			//Check collision only when the ball is going down (for the bar)
 			if(angle < 180)
 			{
-				vely = (aproxsin(angle) * velocity) / 200;
-				if(ball_y + vely*10 > 2000)
+				velY = (aproxSin(angle) * velocity) / 200;
+				if(ballY + velY*10 > 2000)//Check only when it's close to the bar
 				{
-					velx = (aproxcos(angle) * velocity) / 200;
-					j = abs(vely);
-					for(i = 0; i <= MAX(j/20, 1); ++i)
+					velX = (aproxCos(angle) * velocity) / 200;
+					j = abs(velY);
+					for(i = 0; i <= MAX(j/20, 1); ++i)//Check for intermediary positions so that the ball do not go through the bar
 					{
-						nbx = ball_x + (velx/j) * i;
-						nby = ball_y + i;
+						nbx = ballX + (velX/j) * i;
+						nby = ballY + i;
 
 						//check with the top of the bar
-						barBallPos = nbx - bar_x * 10;
+						barBallPos = nbx - barX * 10;
 						if(barBallPos+80 > 0 && barBallPos < 320)//Check for x (+80 for the width of the ball)
 						{
 							if(nby+80 >= 2010 && nby+80 < 2050)//Check for y with 4px error
 							{
 								angle = 360 - angle;
-								ball_y = 1920;
+								ballY = 1920;
 								//Friction
 								if(barBallPos < 120)//120 -> 160-40 (middle of the paddle - middle of the ball)
 								{
@@ -259,10 +412,11 @@ void main(void)
 				}
 			}
 
-			//TODO check brick collision
+			//Bricks collisions
+			//TODO
 
-			ball_x += (aproxcos(angle) * velocity) / 200;
-			ball_y += (aproxsin(angle) * velocity) / 200;
+			ballX += (aproxCos(angle) * velocity) / 200;
+			ballY += (aproxSin(angle) * velocity) / 200;
 		}
 	}
 }
