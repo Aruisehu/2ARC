44469436bdfe9dbb71ba22773501d4429091f1db
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-06-03 20:05:01 +0200

    Prepare level change

--- mainv2.c
+++ mainv2.c
@@ -19,39 +19,36 @@
 //palette data
 const unsigned char palette[16]={ 0x0f,0x16,0x26,0x36,0x0f,0x18,0x28,0x38,0x0f,0x19,0x29,0x39,0x0f,0x1c,0x2c,0x3c };
 const unsigned char maskPpuBase = 0x80;
+const unsigned char currentLevel = 0;
 
 //MetaSprite for the bar
 const unsigned char barMetaSprite[]={
-	0,	0,	0xF0,	1,
-	8,	0,	0xF1,	1,
-	16,	0,	0xF1,	1,
-	24,	0,	0xF2,	1,
-	128
+    0,	0,	0xF0,	1,
+    8,	0,	0xF1,	1,
+    16,	0,	0xF1,	1,
+    24,	0,	0xF2,	1,
+    128
 };
 
 //metatile definitions, one metatile by another, 4 bytes per metatile
 const unsigned char metatiles[]={
-	0x00,0x00,0x00,0x00,
-	0x80,0x81,0x90,0x91,
-	0x82,0x83,0x92,0x93,
-	0x84,0x85,0x94,0x95,
-	0x86,0x87,0x96,0x97
+    0x00,0x00,0x00,0x00,
+    0x80,0x81,0x90,0x91,
+    0x82,0x83,0x92,0x93,
+    0x84,0x85,0x94,0x95,
+    0x86,0x87,0x96,0x97
 };
 
 //metatile attributes, define which palette should be used for a metatile
 const unsigned char metaattrs[]={
-	BGPAL0,
-	BGPAL0,
-	BGPAL1,
-	BGPAL2,
-	BGPAL3
+    BGPAL0,
+    BGPAL0,
+    BGPAL1,
+    BGPAL2,
+    BGPAL3
 };
 
-//Each byte in level Data represents one row.
-//each bit in that byte determine if there is or not a brick in this position
-//ex: 0111 0101 (0x75) means there's no brick in first position, there's one in 2,3,4 position, etc...
-//one brick is 32 pixel wide
-static unsigned char levelData[]= 
+static unsigned char firstLevelData[]= 
 {
     0x00, 0x00, 0xFF, 0xFF,
     0xFF, 0xFF, 0xFF, 0xFF,
@@ -60,8 +57,30 @@ static unsigned char levelData[]=
     0xFF, 0xFF, 0xFF, 0xFF,
     0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00
+    0x00, 0x00,
 };
+
+static unsigned char secondLevelData[]= 
+{
+    0x00, 0x00, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF, 0xFF,
+    0xFF, 0xEF, 0xE0, 0xEF,
+    0xFF, 0xFF, 0xFF, 0xFF,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00,
+};
+
+static unsigned char* allLevels[] =
+{
+    firstLevelData, secondLevelData
+};
+//Each byte in level Data represents one row.
+//each bit in that byte determine if there is or not a brick in this position
+//ex: 0111 0101 (0x75) means there's no brick in first position, there's one in 2,3,4 position, etc...
+//one brick is 32 pixel wide
+static unsigned char levelData[30];
 
 //calculate height of the level in pixels
 #define LEVEL_HEIGHT	(sizeof(levelData)/16*16)
@@ -88,7 +107,7 @@ void updateAttrs()
     ppu_wait_frame();
 }
 
-void updateLineScreen(unsigned int line)
+void updateLineBricks(unsigned int line)
 {
     //line is a number comprise beetween 0 and 30
     static unsigned char vramBuffer[36];
@@ -124,22 +143,26 @@ void updateLineScreen(unsigned int line)
 //funciton like this could be used for collision detection purposes
 unsigned char checkCollisionAndRemoveBrick(signed short x, signed short y)
 {
-	//144 = 18 * 8
-	static unsigned char brick, brickLine, brickBit, mask;
-        
-        brickLine = y>>3; // divide by 8 
-        brickBit = x>>5; // divide by 32
+    //144 = 18 * 8
+    static unsigned char brick, brickLine, brickBit, mask;
 
-        mask = maskPpuBase>>brickBit;
-        brick = levelData[brickLine]&mask;
-	if(brick == mask && brickLine < sizeof(levelData))
-	{
-            mask = ~mask;
-            levelData[brickLine] = levelData[brickLine]&mask;
-            updateLineScreen(brickLine);
-	}
+    brickLine = y>>3; // divide by 8 
+    brickBit = x>>5; // divide by 32
 
-	return brick;
+    mask = maskPpuBase>>brickBit;
+    brick = levelData[brickLine]&mask;
+    if(brick == mask && brickLine < sizeof(levelData))
+    {
+        mask = ~mask; //Invert mask
+        levelData[brickLine] = levelData[brickLine]&mask;
+        updateLineBricks(brickLine);
+    }
+    else 
+    {
+        brick = NULL;
+    }
+
+    return brick;
 }
 
 
@@ -147,9 +170,9 @@ unsigned char checkCollisionAndRemoveBrick(signed short x, signed short y)
 //main function, program starts here
 void main(void)
 {
-	static unsigned char pad,tile,sprite_x,sprite_y;
-	static int i;//For for loop
-	static unsigned char barX = 114;//Current X position of the bar
+    static unsigned char pad,tile,sprite_x,sprite_y;
+    static int i;//For for loop
+    static unsigned char barX = 114;//Current X position of the bar
     static unsigned char barY = 200;//Current Y position of the bar (should never change)
     static unsigned char spr;//Used for sprite drawing
     static unsigned char ballStuck = TRUE;//1 when the ball is on the bar 0 when it's free
@@ -163,30 +186,32 @@ void main(void)
     static signed short barBallPos;//Position of the ball on the bar
     static unsigned short xb1, xb2, yb1, yb2, xa1, xa2, ya1, ya2;
 
-	pal_spr(palette);//set sprite palette
-	pal_bg(palette);//set background palette from an array
-	pal_col(17,0x30);//white color for sprite
+    pal_spr(palette);//set sprite palette
+    pal_bg(palette);//set background palette from an array
+    pal_col(17,0x30);//white color for sprite
 
-	sprite_x=128;
-	sprite_y=120;
+    sprite_x=128;
+    sprite_y=120;
 
-	ppu_on_all();//enable rendering
-        updateAttrs();
-        for (i=0; i <30; ++i)
-        {
-            updateLineScreen(i);       
-            ppu_wait_frame();
-        }
+    memcpy(levelData, allLevels[currentLevel +1], 30);
 
-	delay(60);//delay to show the preloaded part of the level
+    ppu_on_all();//enable rendering
+    updateAttrs();
+    for (i=0; i <30; ++i)
+    {
+        updateLineBricks(i);       
+        ppu_wait_frame();
+    }
 
-	while(1)
-	{
-		ppu_wait_frame();
+    delay(60);//delay to show the preloaded part of the level
 
-		/*
-			Inputs
-		*/
+    while(1)
+    {
+        ppu_wait_frame();
+
+        /*
+           Inputs
+           */
 
         //Get the user's inputs
         pad = pad_poll(0);
@@ -214,7 +239,7 @@ void main(void)
             if(pad&PAD_A)
             {
                 ballStuck = FALSE;
-                ballVelocity = 10;//max 50
+                ballVelocity = 20;//max 50
                 if(pad&PAD_LEFT)
                 {
                     ballDirection = 2;
@@ -231,13 +256,13 @@ void main(void)
         }
 
         /*
-			Ball movement & collisions
-        */
+           Ball movement & collisions
+           */
 
         if(ballStuck == FALSE)
         {
-        	//Border collisions
-        	if(ballY < 128)//Top (8*16)
+            //Border collisions
+            if(ballY < 128)//Top (8*16)
             {
                 ballY = 128;
                 ballDirection = getCollisionBallDirection(ballDirection, TRUE);
@@ -260,7 +285,7 @@ void main(void)
             //Bar collisions
             else if(ballDirection >= 4 && ballDirection <= 12)//Only when the ball is going down
             {
-            	velY = getYMotionFromDirection(ballDirection, ballVelocity);
+                velY = getYMotionFromDirection(ballDirection, ballVelocity);
                 if(ballY + velY > 3150)//Check only when it's close to the bar
                 {
                     velX = getXMotionFromDirection(ballDirection, ballVelocity);
@@ -281,13 +306,13 @@ void main(void)
                                 //Friction
                                 if(barBallPos > 192)//192 -> 256-64 (middle of the paddle - middle of the ball)
                                 {
-                                	ballDirection = ballDirection == 0 ? 15 : ballDirection == 13 ? 13 :  ballDirection - 1;//We don't want the ball to be horizontal
+                                    ballDirection = ballDirection == 0 ? 15 : ballDirection == 13 ? 13 :  ballDirection - 1;//We don't want the ball to be horizontal
                                     //angle -= 10 + (rand8() % 5);
                                 }
                                 else if(barBallPos < 192)
                                 {
-                                	ballDirection = ballDirection == 15 ? 0 : ballDirection == 3 ? 3 : ballDirection + 1;//We don't want the ball to be horizontal
-                                   // angle += 10 + (rand8() % 5);
+                                    ballDirection = ballDirection == 15 ? 0 : ballDirection == 3 ? 3 : ballDirection + 1;//We don't want the ball to be horizontal
+                                    // angle += 10 + (rand8() % 5);
                                 }
                             }
                         }
@@ -300,93 +325,93 @@ void main(void)
             ya1 = ballY>>4;
             xa2 = xa1 + 8;
             ya2 = ya1 + 8;
-			tile=checkCollisionAndRemoveBrick(xa1,ya1);//Collide with brick on ball's top left corner
-			if(tile)
-			{
-				xb1 = xa1>>4<<4;
-				xb2 = xb1+16;
-				yb1 = ya1>>4<<4;
-				yb2 = yb1+16;
-			}
-			if(!tile) 
-			{
-				tile=checkCollisionAndRemoveBrick(xa2,ya1);//Collide with brick on ball's top right corner
-				if(tile)
-				{
-					xb1 = xa2>>4<<4;
-					xb2 = xb1+16;
-					yb1 = ya1>>4<<4;
-					yb2 = yb1+16;
-				}
+            tile=checkCollisionAndRemoveBrick(xa1,ya1);//Collide with brick on ball's top left corner
+            if(tile)
+            {
+                xb1 = xa1>>4<<4;
+                xb2 = xb1+16;
+                yb1 = ya1>>4<<4;
+                yb2 = yb1+16;
             }
             if(!tile) 
-			{
-				tile=checkCollisionAndRemoveBrick(xa1,ya2);//Collide with brick on ball's bottom left corner
-				if(tile)
-				{
-					xb1 = xa1>>4<<4;
-					xb2 = xb1+16;
-					yb1 = ya2>>4<<4;
-					yb2 = yb1+16;
-				}
+            {
+                tile=checkCollisionAndRemoveBrick(xa2,ya1);//Collide with brick on ball's top right corner
+                if(tile)
+                {
+                    xb1 = xa2>>4<<4;
+                    xb2 = xb1+16;
+                    yb1 = ya1>>4<<4;
+                    yb2 = yb1+16;
+                }
             }
             if(!tile) 
-			{
-				tile=checkCollisionAndRemoveBrick(xa2,ya2);//Collide with brick on ball's bottom right corner
-				if(tile)
-				{
-					xb1 = xa2>>4<<4;
-					xb2 = xb1+16;
-					yb1 = ya2>>4<<4;
-					yb2 = yb1+16;
-				}
+            {
+                tile=checkCollisionAndRemoveBrick(xa1,ya2);//Collide with brick on ball's bottom left corner
+                if(tile)
+                {
+                    xb1 = xa1>>4<<4;
+                    xb2 = xb1+16;
+                    yb1 = ya2>>4<<4;
+                    yb2 = yb1+16;
+                }
+            }
+            if(!tile) 
+            {
+                tile=checkCollisionAndRemoveBrick(xa2,ya2);//Collide with brick on ball's bottom right corner
+                if(tile)
+                {
+                    xb1 = xa2>>4<<4;
+                    xb2 = xb1+16;
+                    yb1 = ya2>>4<<4;
+                    yb2 = yb1+16;
+                }
             }
             if(tile)//If we collide with any brick we look where the ball is and bounce accordingly
             {
-            	if(ballDirection == 0 || ballDirection == 8)
-            	{
-            		ballDirection = getCollisionBallDirection(ballDirection, TRUE);
-            	}
-            	else if(ballDirection == 4 || ballDirection == 12)
-            	{
-					ballDirection = getCollisionBallDirection(ballDirection, FALSE);
-            	}
-            	else if (ballDirection > 4 && ballDirection < 12 && ((xa1 >= xb1 && xa2 <= xb2) || (ya2 >= yb1 && ya1 < yb1 && ABS(ya2-yb1) < MIN(ABS(xa1-xb1), ABS(xa2-xb2)))))//Top
-            	{
-            		ballDirection = getCollisionBallDirection(ballDirection, TRUE);
-            	}
-            	else if(ballDirection > 0 && ballDirection < 8 && ((ya1 >= yb1 && ya2 <= yb2) || (xa1 <= xb2 && xa2 > xb2 && ABS(xa1-xb2) < MIN(ABS(ya1-yb1), ABS(ya2-yb2)))))//Right
-            	{
-					ballDirection = getCollisionBallDirection(ballDirection, FALSE);
-            	}
-            	else if((ballDirection < 4 || ballDirection > 12) && ((xa1 >= xb1 && xa2 <= xb2) || (ya1 <= yb2 && ya2 > yb1 && ABS(ya1-yb2) < MIN(ABS(xa1-xb1), ABS(xa2-xb2)))))//Bottom
-            	{
-					ballDirection = getCollisionBallDirection(ballDirection, TRUE);
-            	}
-            	else if(ballDirection > 8 && ((ya1 >= yb1 && ya2 <= yb2) || (xa2 >= xb1 && xa1 < xb1 && ABS(xa2-xb1) < MIN(ABS(ya1-yb1), ABS(ya2-yb2)))))//Left
-            	{
-					ballDirection = getCollisionBallDirection(ballDirection, FALSE);
-            	}
-            	else 
-            	{
-            		//Problem
-            	}
+                if(ballDirection == 0 || ballDirection == 8)
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, TRUE);
+                }
+                else if(ballDirection == 4 || ballDirection == 12)
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, FALSE);
+                }
+                else if (ballDirection > 4 && ballDirection < 12 && ((xa1 >= xb1 && xa2 <= xb2) || (ya2 >= yb1 && ya1 < yb1 && ABS(ya2-yb1) < MIN(ABS(xa1-xb1), ABS(xa2-xb2)))))//Top
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, TRUE);
+                }
+                else if(ballDirection > 0 && ballDirection < 8 && ((ya1 >= yb1 && ya2 <= yb2) || (xa1 <= xb2 && xa2 > xb2 && ABS(xa1-xb2) < MIN(ABS(ya1-yb1), ABS(ya2-yb2)))))//Right
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, FALSE);
+                }
+                else if((ballDirection < 4 || ballDirection > 12) && ((xa1 >= xb1 && xa2 <= xb2) || (ya1 <= yb2 && ya2 > yb1 && ABS(ya1-yb2) < MIN(ABS(xa1-xb1), ABS(xa2-xb2)))))//Bottom
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, TRUE);
+                }
+                else if(ballDirection > 8 && ((ya1 >= yb1 && ya2 <= yb2) || (xa2 >= xb1 && xa1 < xb1 && ABS(xa2-xb1) < MIN(ABS(ya1-yb1), ABS(ya2-yb2)))))//Left
+                {
+                    ballDirection = getCollisionBallDirection(ballDirection, FALSE);
+                }
+                else 
+                {
+                    //Problem
+                }
             }
 
-			//}
+            //}
 
-        	//We move the ball according to its direction and velocity
+            //We move the ball according to its direction and velocity
             ballX += getXMotionFromDirection(ballDirection, ballVelocity);
             ballY += getYMotionFromDirection(ballDirection, ballVelocity);
-        }
+    }
 
-		/*
-		    Drawing
-		*/
+    /*
+       Drawing
+       */
 
-		spr = 0;
+    spr = 0;
 
-		spr = oam_meta_spr(barX, barY, spr, barMetaSprite);//Draw the bar at its coordinates
-		spr = oam_spr((unsigned char)(ballX>>4), (unsigned char)(ballY>>4), BALL_SPR, 3, spr);//Draw the ball at its coordinates (converted from units to pixels)
-	}
+    spr = oam_meta_spr(barX, barY, spr, barMetaSprite);//Draw the bar at its coordinates
+    spr = oam_spr((unsigned char)(ballX>>4), (unsigned char)(ballY>>4), BALL_SPR, 3, spr);//Draw the ball at its coordinates (converted from units to pixels)
+}
 }
